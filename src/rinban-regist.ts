99684bff41d232e6e870893c4c8752ec376526b1
shintaro.itagaki <shintaro.itagaki@dena.com>
2023-10-02 21:12:57 +0900

    add: rinban-regist

--- src/rinban-regist.ts
+++ src/rinban-regist.ts
@@ -1,4 +1,4 @@
-import { addDoc, collection } from "firebase/firestore";
+import { addDoc, collection } from 'firebase/firestore';
 import { LitElement, css, html } from 'lit';
 import { customElement } from 'lit/decorators.js';
 import { db } from './rinban-firestore';
@@ -22,12 +22,24 @@ export class RinbanRegist extends LitElement {
   override render() {
     return html`
       <h1>輪番登録</h1>
-      <label for="name">輪番名</label>
-      <input id="name" type="text" />
-      <label for="members">メンバー</label>
-      <input id="members" type="text" />
-      <label for="period">周期</label>
-      <input id="period" type="text" />
+      <div>
+        <label for="name">輪番名</label>
+        <input id="name" type="text" />
+      </div>
+      <div>
+        <label for="members">メンバー</label>
+        <textarea id="members" rows="5" placeholder="メールアドレスをカンマ区切りで入力してください"></textarea>
+      </div>
+      <div>
+        <label for="repeatNumber">繰り返す間隔</label>
+        <input id="repeatNumber" type="number" value="1"/>
+        <select id="repeatPeriod">
+          <option value="day">日ごと</option>
+          <option value="week" selected>週間ごと</option>
+          <option value="month">か月ごと</option>
+          <option value="year">年ごと</option>
+        </select>
+      </div>
       <button @click=${this._regist} part="button">
         登録
       </button>
@@ -35,22 +47,33 @@ export class RinbanRegist extends LitElement {
   }
 
   private async _regist() {
-    console.log('regist');
+    const name = this.shadowRoot?.getElementById('name') as HTMLInputElement;
+    const membersString = this.shadowRoot?.getElementById(
+      'members'
+    ) as HTMLInputElement;
+    const members = membersString.value.split(',');
+    const repeatNumber = this.shadowRoot?.getElementById(
+      'repeatNumber'
+    ) as HTMLInputElement;
+    const repeatPeriod = this.shadowRoot?.getElementById(
+      'repeatPeriod'
+    ) as HTMLInputElement;
     try {
-      const docRef = await addDoc(collection(db, "rinban"), {
-        first: "Ada",
-        last: "Lovelace",
-        born: 1815
+      const docRef = await addDoc(collection(db, 'rinban'), {
+        name: name.value,
+        members: members,
+        repeatNumber: repeatNumber.value,
+        repeatPeriod: repeatPeriod.value,
       });
-      console.log("Document written with ID: ", docRef.id);
+      console.log('Document written with ID: ', docRef.id);
     } catch (e) {
-      console.error("Error adding document: ", e);
+      console.error('Error adding document: ', e);
     }
   }
 }
 
 declare global {
   interface HTMLElementTagNameMap {
-    'regist': RinbanRegist;
+    regist: RinbanRegist;
   }
 }
